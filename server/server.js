c53711760decc85c8de172fd21fb1590158d5bb1
Atharva Pandey <pandeyatharva665@gmail.com>
2026-08-26 23:19:23 +0530

    Fix Vercel build

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "http://localhost:3000", // frontend origin
+        origin: "", // frontend origin
         credentials: true, // allow cookies to be sent with requests
     })
 );
